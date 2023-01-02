77ca0eac5b8106fd99d050ce8074c5d4a1636d32
Phillip Green II <phillip.green.ii@gmail.com>
2023-01-02 11:31:51 -0500

    21 part working

--- 2022/21/solution.py
+++ 2022/21/solution.py
@@ -9,6 +9,7 @@ def read_lines(file_name, skip_blanks=True):
 from enum import Enum
 from typing import NamedTuple, List, Iterator, Set, Dict, Optional, Tuple, Union
 
+
 class ValueMonkey(NamedTuple):
     value:int
 
@@ -17,7 +18,9 @@ class OperationMonkey(NamedTuple):
     param1:str
     param2:str
 
-def parse_monkey(line:str)->Tuple[str,Union[ValueMonkey,OperationMonkey]]:
+Monkey = Union[ValueMonkey,OperationMonkey]
+
+def parse_monkey(line:str)->Tuple[str,Monkey]:
     parts = line.split(':')
     name = parts[0]
     parts = parts[1].split()
@@ -27,36 +30,108 @@ def parse_monkey(line:str)->Tuple[str,Union[ValueMonkey,OperationMonkey]]:
         return (name, OperationMonkey(parts[1],parts[0],parts[2]))
     
 
-def read_monkeys(file_name:str)->Dict[str,Union[ValueMonkey,OperationMonkey]]:
+def read_monkeys(file_name:str)->Dict[str,Monkey]:
     return { name:monkey for name,monkey in map(parse_monkey,read_lines(file_name)) }
-
-
-def determine_value(monkeys,monkey_name) -> int:
-    v = monkeys[monkey_name]
-    if isinstance(v,ValueMonkey):
-        return v.value
-    else:
-        if v.operation == '+':
-            return determine_value(monkeys,v.param1) + determine_value(monkeys,v.param2)
-        elif v.operation == '-':
-            return determine_value(monkeys,v.param1) - determine_value(monkeys,v.param2)
-        elif v.operation == '*':
-            return determine_value(monkeys,v.param1) * determine_value(monkeys,v.param2)
-        elif v.operation == '/':
-            return determine_value(monkeys,v.param1) // determine_value(monkeys,v.param2)
-        else: 
-            raise Exception(f"Unknown operation: {v}")
 
 
 def part1(file_name):
     monkeys = read_monkeys(file_name)
-    print(monkeys)
-    # print(determine_value(monkeys,'dbpl'))
+    tree = build_tree(monkeys, 'root')
+    # print(tree)
+    tree = reduce_tree(tree)
+    # print(tree)
+    return tree.value
 
-    result = determine_value(monkeys, 'root')
-    return result
+Node = Union["ValueNode","OperationNode", "UnknownValueNode"]
 
+class ValueNode(NamedTuple):
+    value:int
+
+class OperationNode(NamedTuple):
+    operation:str
+    node1:Node
+    node2:Node
+
+class UnknownValueNode(NamedTuple):
+    name:str
+
+def monkey2node(monkeys:Dict[str,Monkey], name:str)->Node:
+    if name not in monkeys:
+        return UnknownValueNode(name)
+    monkey = monkeys[name]
+    if isinstance(monkey,ValueMonkey):
+        return ValueNode(monkey.value)
+    else:
+        return OperationNode(monkey.operation,monkey2node(monkeys,monkey.param1),monkey2node(monkeys,monkey.param2))
+
+def build_tree(monkeys:Dict[str,Monkey], root:str)->Node:
+    return monkey2node(monkeys,root)
     
+def reduce_tree(tree:Node)->Node:
+    if isinstance(tree,ValueNode):
+        return tree
+    if isinstance(tree,UnknownValueNode):
+        return tree
+
+    n1 = reduce_tree(tree.node1)
+    n2 = reduce_tree(tree.node2)
+    
+    # check for partial evalution
+    if isinstance(n1,OperationNode) or isinstance(n2,OperationNode)\
+        or isinstance(n1,UnknownValueNode) or isinstance(n2,UnknownValueNode):
+        if n1 == tree.node1 and n2 == tree.node2:
+            return tree
+        else:
+            return OperationNode(tree.operation, n1, n2)
+    
+    # full evaluation
+    o = tree.operation
+    v1 = n1.value
+    v2 = n2.value
+    if o == '+':
+        v = v1 + v2
+    elif o == '-':
+        v = v1 - v2
+    elif o == '*':
+        v = v1 * v2
+    elif o == '/':
+        v = v1 // v2
+    else: 
+        raise Exception(f"Unknown operation: {o}")
+    return ValueNode(v)
+
+operator_opposite = {
+    '+':'-',
+    '-':'+',
+    '*':'/',
+    '/':'*'
+}
+
+def unwrap_operation(unknown:Node, value:Node)->Tuple[Node,Node]:
+    if isinstance(unknown,UnknownValueNode):
+        return unknown,value
+    if isinstance(unknown.node1,ValueNode):
+        v=unknown.node1
+        op=unknown.node2
+    else:
+        v=unknown.node2
+        op=unknown.node1
+    return op,OperationNode(operator_opposite[unknown.operation],value,v)
+
+def solve_for_unknown(tree:Node)->Node:
+    if tree.operation != '=':
+        raise Exception(f"operation ({tree.operation}) must be '='")
+    if isinstance(tree.node1,ValueNode):
+        value = tree.node1
+        unknown = tree.node2
+    else:
+        value = tree.node2
+        unknown = tree.node1
+    while not isinstance(unknown,UnknownValueNode):
+        unknown, value = unwrap_operation(unknown, value)
+    value = reduce_tree(value)
+    return value
+
 """
 Due to some kind of monkey-elephant-human mistranslation, you seem to have misunderstood a few key details about the riddle.
 
@@ -69,16 +144,33 @@ In the above example, the number you need to yell to pass root's equality test i
 What number do you yell to pass root's equality test?
 """
 def part2(file_name):
-    pass
+    monkeys = read_monkeys(file_name)
+    # update root operation
+    monkeys['root'] = OperationMonkey('=',monkeys['root'].param1,monkeys['root'].param2)
+    # remove value for humn
+    del monkeys['humn']
+
+    tree = build_tree(monkeys, 'root')
+    # print("initial tree")
+    # print(tree.node1)
+    # print(tree.node2)
+    tree = reduce_tree(tree)
+    # print("reduced tree")
+    # print(tree.node1)
+    # print(tree.node2)
+    tree = solve_for_unknown(tree)
+    # print("solved tree")
+    # print(tree)
+    return tree.value
 
 print("part 1")
-# 15
+# 152
 print(part1('example.txt'))
-# 3068
+# 157714751182692
 print(part1('input.txt'))
 
 print("part 2")
 # 301
 print(part2('example.txt'))
-# ?
-# print(part2('input.txt'))
+# 9879574614298
+print(part2('input.txt'))
